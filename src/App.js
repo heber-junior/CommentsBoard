417170cb668b82731ca696d2bab1aff4914c8ed8
Heber Junior <junior.hbg@gmail.com>
2019-01-08 00:07:03 -0200

    Comments board

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Comments from './Comments';
+import NewComment from './NewComment';
 
 class App extends Component {
+ 
+  state = {
+    comments: []
+  }
+
+  sendComment = comment => {
+    this.setState({comments: [...this.state.comments, comment]});
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <NewComment sendComment={this.sendComment}/>
+        {JSON.stringify(this.state)}
+        <Comments comments={this.state.comments}/>
       </div>
     );
   }
---
+++ src/NewComment.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+
+class NewComment extends Component {
+
+    state = {
+        newComment: ''
+    }
+
+    commentHandler = event => {
+        this.setState({newComment: event.target.value});
+    }
+
+    sendComment = () => {
+        this.props.sendComment(this.state.newComment);
+        this.setState({newComment: ''});
+    }
+
+    render() {
+        return (
+            <div>
+                <textarea onChange={this.commentHandler} value={this.state.newComment}></textarea>
+                <button onClick={this.sendComment}>Enviar</button>
+                {JSON.stringify(this.state)}
+            </div>
+        );
+    }
+}
+
+export default NewComment;
